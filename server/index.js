83edf8f3f62680c239d08195cf8764c54cc99f7f
israel <israelpinto276@gmail.com>
2024-12-17 23:16:20 +0000

    Dec 17, 2024, 7:16 PM

--- server/index.js
+++ server/index.js
@@ -1,15 +1,32 @@
-import express from 'express'
-import userRouter from "./routes/user.route.js"
+import express from "express";
+import connectDB from "./lib/connectDB.js";
+import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
+import commentRouter from "./routes/comment.route.js";
+import webhookRouter from "./routes/webhook.route.js";
 
-const app = express()
+const app = express();
+app.use(express.json())
 
 // app.get("/test", (req, res) => {
 //     res.status(200).send('it works!')
 // })
 
-app.use("/users", userRouter)
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+app.use("/webhooks", webhookRouter);
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({
+        message: error.message || "Something went wrong!",
+        status: error.status,
+        stack: error.stack
+    })
+})
 
 app.listen(3000, () => {
-    console.log('server is running');
-    
-})
+    connectDB();
+    console.log("server is running");
+});
