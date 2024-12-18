04f92442573fd894ed4c1ef59a0ce1996e47c4bc
israel <israelpinto276@gmail.com>
2024-12-18 01:02:01 +0000

    Dec 17, 2024, 9:02 PM

--- server/controllers/webhook.controller.js
+++ server/controllers/webhook.controller.js
@@ -1,3 +1,11 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const clerkWebHook = async (req, res) => {
-    
+    const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET
+
+    if (!WEBHOOK_SECRET) {
+        throw new Error("Webhook secret needed!");
+        
+    }
 }
